97f1a4ccfd7c533997be4f10389e83281d424687
MAPrinsloo <notmattherwprinsloo@gmail.com>
2023-06-04 11:23:21 +0200

    Clocked Out - V2 //-----------------------------------------------------------------------// //Files changed Categories.kt Goals.kt AndroidManifest.xml build.gradle //-----------------------------------------------------------------------// //Comments Completed my assigned work. Committing those files.

--- app/src/main/java/com/example/clockedout/Categories.kt
+++ app/src/main/java/com/example/clockedout/Categories.kt
@@ -1,24 +1,109 @@
 package com.example.clockedout
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.clockedout.databinding.ActivityCategoriesBinding
 
 
 class Categories : AppCompatActivity() {
+    //private val binding = ActivityCategoriesBinding.inflate(layoutInflater)
+    private var arrCat = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_categories)
-        val spCategories = findViewById<Spinner>(R.id.spCategories)
-        //
-        val arrCategories = arrayOf<String>("Categories", "SampleCat1", "SampleCat2")
+        //----------------------------------------------------------------------------------------//
+        //Binding
+        val binding = ActivityCategoriesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Populating spinner
+        PopulateSpinner(binding)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrCategories)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spCategories.adapter = adapter
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        //----------------------------------------------------------------------------------------//
+        //Create Button
+        binding.btnCreate.setOnClickListener()
+        {
+            try {
+                var Category : String = binding.etCatName.text.toString()
+                Capture(Category)
+                PopulateSpinner(binding)
+            }
+            catch (e: java.lang.IllegalArgumentException)
+            {
+                Toast.makeText(this,"Error Occurred, Ensure all values are entered correctly.", Toast.LENGTH_LONG).show()
+            }
+        }
+        //----------------------------------------------------------------------------------------//
+        //Delete Button
+        binding.btnDelete.setOnClickListener()
+        {
+            try {
+                val selectedCategory: String = binding.spCategories.selectedItem.toString()
+                Delete(selectedCategory);
+                PopulateSpinner(binding)
+            }
+            catch (e: java.lang.IllegalArgumentException)
+            {
+                Toast.makeText(this,"Error Occurred, Ensure all values are entered correctly.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
+    //----------------------------------------------------------------------------------------//
+    private fun Capture(Category: String)
+    {
+        //
+        val sharedCategoryCounter = getSharedPreferences("SharedCatCounter",Context.MODE_PRIVATE)
+        var counterEditor = sharedCategoryCounter.edit()
+        var counter : Int = sharedCategoryCounter.getInt("Counter",0).toInt()
+        counter += 1
+        counterEditor.putInt("Counter", counter)
+        counterEditor.commit()
+        //
+        val sharedPreference =  getSharedPreferences("SharedCategories",Context.MODE_PRIVATE)
+        var catEditor = sharedPreference.edit()
+        catEditor.putString(counter.toString(), Category)
+        catEditor.commit()
+        Toast.makeText(this,"Category Captured", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun Delete(itemToDelete: String)
+    {
+        val sharedPreference =  getSharedPreferences("SharedCategories",Context.MODE_PRIVATE)
+        var catEditor = sharedPreference.edit()
+        var counter: Int = 0
+
+        catEditor.clear()
+        for (category in arrCat)
+        {
+            if(!category.equals(itemToDelete))
+            {
+                counter += 1
+                catEditor.putString(counter.toString(),category)
+                catEditor.commit()
+            }
+        }
+        arrCat.clear()
+        val sharedCategoryCounter = getSharedPreferences("SharedCatCounter",Context.MODE_PRIVATE)
+        var counterEditor = sharedCategoryCounter.edit()
+        counterEditor.putInt("Counter", counter)
+        counterEditor.commit()
+    }
+
+    private  fun PopulateSpinner(binding: ActivityCategoriesBinding)
+    {
+        val sharedPreferences = getSharedPreferences("SharedCategories", Context.MODE_PRIVATE)
+        val allEntries: Map<String, *> = sharedPreferences.all
+        for ((key, value) in allEntries) {
+            arrCat.add(value.toString())
+        }
+        //Populating the spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, arrCat)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spCategories.adapter = adapter
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
 }
+
--- app/src/main/java/com/example/clockedout/Goals.kt
+++ app/src/main/java/com/example/clockedout/Goals.kt
@@ -1,11 +1,77 @@
 package com.example.clockedout
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Paint.Cap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.clockedout.databinding.ActivityGoalsBinding
+import com.example.clockedout.databinding.ActivityMainBinding
 
 class Goals : AppCompatActivity() {
+    //Static variables to be called elsewhere
+    //https://marketsplash.com/tutorials/kotlin/kotlin-static-variable/#link1
+    /*
+    companion object {
+        var MinHours: Int = 0
+        var MaxHours: Int = 0
+        public fun InitializeGoals ()
+        {
+            MinHours = g
+        }
+    }
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_goals)
+        //----------------------------------------------------------------------------------------//
+        //Binding
+        val binding = ActivityGoalsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var minHours: Int = 0
+        var maxHours: Int = 0
+
+        //----------------------------------------------------------------------------------------//
+        //Set Goals Button
+        binding.btnSetGoals.setOnClickListener()
+        {
+            try {
+                minHours = binding.etMinHours.text.toString().toInt()
+                maxHours = binding.etMaxHours.text.toString().toInt()
+                if (minHours > 0 && maxHours > 0)
+                {
+                    CaptureGoals(minHours, maxHours)
+                }
+                if (minHours <= 0)
+                {
+                    binding.etMinHours.error = "Must be greater than 0"
+                }
+                if (maxHours <= 0)
+                {
+                    binding.etMaxHours.error = "Must be greater than 0"
+                }
+            }
+            catch (e: java.lang.IllegalArgumentException)
+            {
+                Toast.makeText(this,"Error Occurred, Ensure all values are entered correctly.", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+    //--------------------------------------------------------------------------------------------//
+
+    private fun CaptureGoals(minHours: Int, maxHours: Int)
+    {
+        //Shared preferences for persisting data --v
+        //https://youtu.be/fJEFZ6EOM9o
+
+
+        val sharedPreference =  getSharedPreferences("SharedPrefs",Context.MODE_PRIVATE)
+        var editor = sharedPreference.edit()
+
+        editor.putInt("MinHours", minHours)
+        editor.putInt("MaxHours", maxHours)
+        editor.commit()
+        Toast.makeText(this,"Goals Set", Toast.LENGTH_SHORT).show()
     }
 }
